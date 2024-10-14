fc5acad8213aedbea3e7b7ce5e78a9784c56c070
Ai3011 <161729375+Ai3011@users.noreply.github.com>
2024-10-14 10:11:13 -0400

    Update CSV.h
    update all project

--- CSV.h
+++ CSV.h
@@ -4,4 +4,4 @@
 void unlock(const filesystem::path& directory, const std::string tableName);
 void writeToCsv(const filesystem::path& filename, List<List<string>>& data, string table_name, string& schem_name);
 void readFromCsv(const filesystem::path& filename, List<List<string>>& data, string table_name, string& schem_name);
-void appendToCsv(const filesystem::path & filename, List<string> & newRow, string table_name, string & schem_name); 
+void appendToCsv(const filesystem::path& filename, List<string>& newRow, string table_name, string& schem_name);
