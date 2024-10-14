b3ae668557237d989549dc0e649d86845f7b7104
Ai3011 <161729375+Ai3011@users.noreply.github.com>
2024-10-14 10:11:49 -0400

    Update parsJS.cpp

--- parsJS.cpp
+++ parsJS.cpp
@@ -1,179 +1,194 @@
-#include <stdio.h> // Подключаем библиотеку для работы с вводом-выводом
-#include <stdlib.h> // Подключаем библиотеку для работы с памятью
-#include <stdint.h> // Подключаем библиотеку для работы с целыми типами фиксированного размера
-#include <string.h> // Подключаем библиотеку для работы со строками
-#include <iostream> // Подключаем библиотеку для ввода-вывода в C++
-#include <string> // Подключаем библиотеку для работы со строками в C++
-#include "Struct.h" // Подключаем пользовательский заголовочный файл Struct.h
+#include <iostream> 
+#include <string>
+#include "structures.h"
+#include <fstream>
+#include <cstdint>
 
-using namespace std; // Используем пространство имен std для удобства
+using namespace std;
 
-// Функция для загрузки содержимого JSON-файла
 char* LoadJsonFile(const char* fname, int64_t& size) {
-    FILE* fp = fopen(fname, "rb"); // Открываем файл для чтения в бинарном режиме
+    // Открываем файл в бинарном режиме, сразу перемещаемся в конец
+    std::ifstream file(fname, std::ios::binary | std::ios::ate);
 
-    // Проверка успешности открытия файла
-    if (!fp) {
-        printf("Ошибка: не удалось открыть файл.\n"); // Выводим сообщение об ошибке
-        exit(1); // Завершаем программу
+    // Проверяем, что файл открыт
+    if (!file) {
+        std::cerr << "Ошибка: не удалось открыть файл." << std::endl;
+        exit(1);
     }
 
-    int64_t file_length = 0; // Переменная для хранения длины файла
+    // Получаем размер файла
+    size = file.tellg();
+    file.seekg(0, std::ios::beg); // Возвращаем указатель в начало файла
 
-    fseek(fp, 0, SEEK_END); // Перемещаем указатель в конец файла
-    file_length = ftell(fp); // Получаем длину файла
-    fseek(fp, 0, SEEK_SET); // Перемещаем указатель обратно в начало файла
+    // Выделяем память под содержимое файла + 1 для завершающего нулевого символа
+    char* content = new char[size + 1];
 
-    // Выделяем память для содержимого файла
-    char* content = new char[file_length + 1];
-    fread(content, 1, file_length, fp); // Читаем содержимое файла в буфер
-    content[file_length] = '\0'; // Добавляем нуль-терминатор для корректного окончания строки
+    // Читаем содержимое файла
+    if (!file.read(content, size)) {
+        std::cerr << "Ошибка: не удалось прочитать файл." << std::endl;
+        delete[] content; // Освобождаем память в случае ошибки
+        exit(1);
+    }
 
-    fclose(fp); // Закрываем файл
-    size = file_length; // Устанавливаем размер содержимого
+    // Добавляем завершающий нулевой символ для корректной работы с C-строкой
+    content[size] = '\0';
 
-    return content; // Возвращаем загруженное содержимое
+    // Закрываем файл
+    file.close();
+
+    return content;
 }
 
-// Функция для извлечения строки из содержимого
+
+
 string take_string(char* cont, int& index) {
-    string content; // Переменная для хранения извлеченной строки
-    bool get_content = 0; // Флаг для контроля извлечения содержимого
-    bool geting_content = 0; // Флаг для контроля нахождения внутри строки
+    string content;
+    bool get_content = 0;
+    bool geting_content = 0;
 
     while (true) {
-        if (get_content) break; // Выходим из цикла, если строка извлечена
-        else if (!get_content) {
-            if (cont[index] == '"') { // Если встречаем кавычку
-                if (geting_content) get_content = true; // Если уже внутри строки, устанавливаем флаг
-                else geting_content = true; // Входим в строку
-            }
-            else if (geting_content) content.push_back(cont[index]); // Добавляем символ в извлекаемую строку
+        if (get_content) {
+            break;
+        }else if (!get_content) {
+            if (cont[index] == '"') {
+                if (geting_content){
+                    get_content = true;
+                }else geting_content = true;
+            }else if (geting_content) {
+                content.push_back(cont[index]);
+            }    
         }
-        index++; // Переходим к следующему символу
+        index++;
     }
-    return content; // Возвращаем извлеченную строку
+    return content;
 }
 
-// Функция для извлечения списка из содержимого
 string take_list(char* cont, int& index) {
-    string content; // Переменная для хранения извлеченного списка
-    bool get_content = 0; // Флаг для контроля извлечения содержимого
-    bool geting_content = 0; // Флаг для контроля нахождения внутри списка
+    string content;
+    bool get_content = 0;
+    bool geting_content = 0;
 
     while (true) {
-        if (get_content) break; // Выходим из цикла, если список извлечен
-        else if (!get_content) {
-            if (cont[index] == '[' || cont[index] == ']') { // Если встречаем квадратные скобки
-                if (geting_content) get_content = true; // Если уже внутри списка, устанавливаем флаг
-                else geting_content = true; // Входим в список
+        if (get_content){ 
+            break;
+        }else if (!get_content) {
+            if (cont[index] == '[' || cont[index] == ']') {
+                if (geting_content){ 
+                    get_content = true;
+                }else geting_content = true;
+            }else if (geting_content)   { 
+                content.push_back(cont[index]);
             }
-            else if (geting_content) content.push_back(cont[index]); // Добавляем символ в извлекаемый список
         }
-        index++; // Переходим к следующему символу
+
+        index++;
     }
-    return content; // Возвращаем извлеченный список
+    return content;
 }
 
-// Функция для извлечения целого числа из содержимого
+
 int take_int(char* cont, int& index) {
-    int content = 0; // Переменная для хранения извлеченного числа
+    int content = 0;
 
-    bool get_content = 0; // Флаг для контроля извлечения содержимого
-    bool geting_content = 0; // Флаг для контроля нахождения внутри числа
-    bool geteted_one = 0; // Флаг для контроля, было ли извлечено хотя бы одно число
+    bool get_content = 0;
+    bool geting_content = 0;
+    bool geteted_one = 0;
 
     while (true) {
-        if (get_content) break; // Выходим из цикла, если число извлечено
+        if (get_content){ 
+            break;
+        }
         if (!get_content) {
-            if ('0' > cont[index] || '9' < cont[index]) { // Если символ не цифра
-                if (geting_content && geteted_one) get_content = true; // Если находимся внутри числа и уже извлекли хотя бы одну цифру
-                else geting_content = true; // Входим в число
+            if ('0' > cont[index] || '9' < cont[index]) {
+                if (geting_content && geteted_one){ 
+                    get_content = true;
+                }else geting_content = true;
+            } else if (geting_content) {
+                geteted_one = true;
+                content = content * 10 + (cont[index] - '0');
             }
-            else if (geting_content) { // Если находимся внутри числа
-                geteted_one = true; // Устанавливаем флаг, что извлекли цифру
-                content = content * 10 + (cont[index] - '0'); // Обновляем число
-            }
+
         }
-        index++; // Переходим к следующему символу
+
+        index++;
     }
-    return content; // Возвращаем извлеченное число
+    return content;
 }
 
-// Функция для конвертации строки в список строк
 List<string> convert_string(string str) {
-    List<string> res; // Результирующий список
-    string name; // Переменная для хранения текущего значения
+    List<string> res;
+    string name;
 
-    bool geting = false; // Флаг для контроля извлечения строки
+    bool geting = false;
 
-    int index = 0; // Индекс текущего символа
+    int index = 0;
     while (index < str.length()) {
-        if (str[index] == '"') { // Если встречаем кавычку
-            if (!geting) geting = true; // Входим в строку
-            else {
-                geting = false; // Выходим из строки
-                res.push(name); // Добавляем извлеченное значение в результат
-                name = ""; // Очищаем переменную для следующей строки
+        if (str[index] == '"') {
+            if (!geting){ 
+                geting = true;
+            } else {
+                geting = false;
+                res.push(name);
+                name = "";
             }
+        } else if (geting) {
+            name.push_back(str[index]);
         }
-        else if (geting) {
-            name.push_back(str[index]); // Добавляем символ в текущее значение
-        }
-        index++; // Переходим к следующему символу
+        index++;
     }
 
-    return res; // Возвращаем результирующий список
+    return res;
 }
 
-// Функция для извлечения пар из содержимого
 void take_pairs(char* cont, int& index, int size, HashTable<List<string>>& tables, List<string>& tables_names, string& schem_name, int& tuples_limit) {
-    string name; // Переменная для хранения текущего имени
-    string content; // Переменная для хранения содержимого
-    string table_name; // Переменная для хранения имени таблицы
-    string columns; // Переменная для хранения столбцов
+    string name;
+    string content;
+    string table_name;
+    string columns;
 
-    bool get_name = 0; // Флаг для контроля извлечения имени
-    bool geting_name = 0; // Флаг для контроля нахождения внутри имени
+    bool get_name = 0;
+    bool geting_name = 0;
 
-    while (index <= size) { // Проходим по содержимому
+    while (index <= size) {
         if (!get_name) {
-            if (cont[index] == '"') { // Если встречаем кавычку
-                if (geting_name) get_name = true; // Если уже внутри имени, устанавливаем флаг
-                else geting_name = true; // Входим в имя
+            if (cont[index] == '"') {
+                if (geting_name){ 
+                    get_name = true;
+                } else geting_name = true;
+            } else if (geting_name) {
+                name.push_back(cont[index]);
             }
-            else if (geting_name) name.push_back(cont[index]); // Добавляем символ в текущее имя
-        }
-        else {
-            // Проверка на ключи и извлечение значений
-            if (name == "name") schem_name = take_string(cont, index); // Извлекаем имя схемы
-            else if (name == "tuples_limit") tuples_limit = take_int(cont, index); // Извлекаем лимит кортежей
-            else if (name == "structure") { // Если ключ "structure"
-                while (cont[index] != '}') { // Пока не встретим закрывающую фигурную скобку
-                    table_name = take_string(cont, index); // Извлекаем имя таблицы
-                    columns = take_list(cont, index); // Извлекаем список столбцов
-                    List<string> columns_li = convert_string(columns); // Конвертируем список в формат List<string>
-                    tables_names.push(table_name); // Добавляем имя таблицы в список имен
-                    tables.Add(table_name, columns_li); // Добавляем таблицу в хэш-таблицу
+        } else {
+            if (name == "name") { 
+                schem_name = take_string(cont, index);
+            } else if (name == "tuples_limit"){
+                 tuples_limit = take_int(cont, index);
+            } else if (name == "structure") {
+                while (cont[index] != '}') {
+                    table_name = take_string(cont, index);
+                    columns = take_list(cont, index);
+                    List<string> columns_li = convert_string(columns);
+                    tables_names.push(table_name);
+                    tables.Add(table_name, columns_li);
                     while (cont[index] != '"' && cont[index] != '}') {
-                        index++; // Пропускаем символы до закрывающей кавычки или фигурной скобки
+                        index++;
                     }
                 }
             }
-            get_name = false; // Сбрасываем флаги
+            get_name = false;
             geting_name = false;
-            name = ""; // Очищаем имя для следующей пары
+            name = "";
         }
-        index++; // Переходим к следующему символу
+        index++;
     }
 }
 
-// Главная функция для обработки JSON
+
 void getJSON(HashTable<List<string>>& tables, List<string>& tables_names, string& schem_name, int& limit) {
-    int64_t size; // Переменная для хранения размера содержимого
-    char* data = LoadJsonFile("shema1.json", size); // Загружаем JSON-файл
+    int64_t size;
+    char* data = LoadJsonFile("shema.json", size);
 
-    int i = 0; // Индекс для чтения содержимого
+    int i = 0;
 
-    take_pairs(data, i, size, tables, tables_names, schem_name, limit); // Извлекаем пары из содержимого
+    take_pairs(data, i, size, tables, tables_names, schem_name, limit);
 }
