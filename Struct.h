eb5777f40d3133f918c3d9dc96e73b4c52c5ecb9
Ai3011 <161729375+Ai3011@users.noreply.github.com>
2024-10-14 10:14:28 -0400

    Update Struct.h
    update all project

--- Struct.h
+++ Struct.h
@@ -1,120 +1,311 @@
-#ifndef DATA_STRUCTURES_H // Защита от множественного включения
-#define DATA_STRUCTURES_H
-
 #include <string>
-#include <type_traits>
-#include <stdexcept> // Для работы с исключениями
 
 using namespace std;
 
-// Шаблон структуры для узла двусвязного списка
 template <typename T>
-struct NodeD {
-    T data; // Данные, хранящиеся в узле
-    NodeD* next = nullptr; // Указатель на следующий узел
-    NodeD* prev = nullptr; // Указатель на предыдущий узел
+struct NodeD{
+    T data;
+    NodeD* next = nullptr;
+    NodeD* prev = nullptr;
 };
 
-// Шаблон структуры для двусвязного списка
 template <typename T>
-struct List {
-    int length = 0; // Длина списка
-    NodeD<T>* head = new NodeD<T>; // Указатель на "голову" списка
-    NodeD<T>* tail = head; // Указатель на "хвост" списка (последний узел)
+struct List{
+    int length = 0;
+    NodeD<T>* head = new NodeD<T>;
+    NodeD<T>* tail = head;
 
-    // Метод добавления нового узла в конец списка
-    void push(T data);
-    
-    // Перегрузка оператора [] для доступа к элементам списка
-    T operator[](int index);
+    void push(T data){
+        NodeD<T>* new_node = new NodeD<T>{data, nullptr, tail};
+        tail -> next = new_node;
+        tail = new_node;
+        length += 1;
+    }
 
-    // Метод удаления последнего узла списка
-    T pop();
+    T operator[](int index){
+        return get(index);
+    }
 
-    // Метод получения элемента по индексу
-    T get(int index);
+    T pop(){
+        if (length == 0) {
+            throw runtime_error("List is empty");
+        }
 
-    // Метод поиска индекса элемента
-    int find(T data);
+        NodeD<T>* last = tail;
 
-    // Метод удаления узла по индексу
-    void del(int index);
+        T data = last -> data;
+        tail -> prev -> next = nullptr;
+        tail = tail -> prev;
 
-    // Метод удаления узла по значению
-    void remove(T data);
+        length --;
 
-    // Метод вставки узла по индексу
-    void insert(T data, int index);
+        delete last;
+        return data;
+    }
 
-    // Метод очистки списка
-    void clear();
+    T get(int index){
+        if (index >= length || index < 0) {
+            throw runtime_error("Out of range");
+        }
+
+        NodeD<T>* tek = head -> next;
+        while (index != 0){
+            tek = tek -> next;
+            index --;
+        }
+        return tek -> data;
+    }
+
+    int find(T data){
+        
+        for (int index = 0; index < length; index++){
+            if (get(index) == data){
+                return index;
+            }
+        }
+
+        return -1;
+    }
+
+    void del(int index){
+        if (index >= length || index < 0) {
+            throw runtime_error("Out of range");
+        }
+        if (index == length - 1) {
+            pop();
+        }
+
+        NodeD<T>* tek = head -> next;
+        while (index != 0){
+            tek = tek -> next;
+            index --;
+        }
+
+        NodeD<T>* pred = tek -> prev;
+        pred -> next = tek -> next;
+        pred -> next -> prev = pred;
+
+        delete tek;
+        length --;
+
+    }
+
+    void remove(T data){
+        int index = find(data);
+
+        if(index == -1) {
+            throw runtime_error("No value");
+        }
+        del(index);
+    }
+
+    void insert(T data, int index){
+        if (index > length || index < 0) {
+            throw runtime_error("Out of range");
+        }
+        if (index == length){
+            push(data);
+        } else if (index >= 0){
+            NodeD<T>* new_node = new NodeD<T>;
+            new_node -> data = data;
+            NodeD<T>* tek = head -> next;
+
+            while (index != 0){
+                tek = tek -> next;
+                index --;
+            }
+
+            new_node -> prev = tek -> prev;
+            tek -> prev -> next = new_node;
+            tek -> prev = new_node;
+            new_node -> next = tek;
+            length++;
+        }
+
+    }
+
+    void clear(){
+        NodeD<T>* prev;
+        NodeD<T>* tek = tail;
+        while (tek != head){
+            prev = tek ->prev;
+            delete tek;
+            tek = prev;
+        }
+        delete tek;
+    }
 };
 
-// Шаблон структуры для массива с логическим состоянием
+
 template <typename T>
-struct Array {
-    struct Node { // Вложенная структура для элемента массива
-        T value; // Значение
-        bool state = false; // Логическое состояние: занято или нет
+struct Array{
+
+    struct Node{
+        T value;
+        bool state = false;
     };
+    int arSize;
+    Node* head;
 
-    int arSize; // Размер массива
-    Node* head; // Указатель на массив элементов
+    Array(int size){
+        arSize = size;
+        head = new Node[size];
+    }
 
-    // Конструктор массива
-    Array(int size);
+    void set(T data, int index){
+        if (index >= arSize) {
+            throw runtime_error("Out of range");
+        }
+        head[index].value = data;
+        head[index].state = true;
+    }
 
-    // Метод установки значения по индексу
-    void set(T data, int index);
+    T operator[](int index){
+        if (index >= arSize) {
+            throw runtime_error("Out of range");
+        }
+        if (!head[index].state) {
+            throw runtime_error("No value");
+        }
+        return head[index].value;
+    }
 
-    // Перегрузка оператора [] для доступа к элементам массива
-    T operator[](int index);
+    void del(int index){
+        if (index >= arSize) {
+            throw runtime_error("Out of range");
+        }
+        if (!head[index].state) {
+            throw runtime_error("No value");
+        }
+        head[index].state = false;
+    }
 
-    // Метод удаления значения по индексу
-    void del(int index);
+    int find(T data){
+        for (int i = 0; i < arSize; i++){
+            if(!head[i].state) {
+                continue;
+            }
 
-    // Метод поиска индекса элемента по значению
-    int find(T data);
+            if(head[i].value == data){
+                return i;
+            }
+        }
 
-    // Метод удаления значения по его содержимому
-    void remove(T data);
+        return -1;
+    }
 
-    // Деструктор для освобождения памяти
-    ~Array();
+    void remove(T data){
+        int index = find(data);
+        if  (index == -1) {
+            throw runtime_error("No value");
+        }
+        head[index].state = false;
+    }
+
+    ~Array(){
+        delete[] head;
+    }
+
 };
 
-// Шаблон структуры для узла хэш-таблицы
 template <typename T>
-struct NodeH {
-    string key; // Ключ
-    T value; // Значение
-    bool state = false; // Состояние: занято или нет
-    bool deleted = false; // Состояние: удалено или нет
+struct NodeH{
+    string key;
+    T value;
+    bool state = false;
+    bool deleted = false;
 };
 
-// Шаблон структуры для хэш-таблицы
+
 template <typename T>
-struct HashTable {
-    int size = 8; // Начальный размер хэш-таблицы
-    NodeH<T>* arr = new NodeH<T>[size]; // Массив узлов хэш-таблицы
+struct HashTable{
 
-    // Деструктор для освобождения памяти
-    ~HashTable();
+    int size = 8;
 
-    // Хэш-функция для вычисления индекса
-    int hashFunc(string key);
+    NodeH<T>* arr = new NodeH<T>[size];
 
-    // Метод для добавления элемента в хэш-таблицу
-    void Add(string key, T value);
+    ~HashTable(){
+        delete [] arr;
+    }
 
-    // Метод для изменения размера хэш-таблицы
-    void Resize();
+    int hashFunc(string key){
+        int hash_result = 0;
+        for (int i = 0; i < key.size(); i++){
+            hash_result = ((size - 1) * hash_result + key[i]) % size;
+            hash_result = (hash_result * 2 + 1) % size;
+        }
+        return hash_result;
+    }
 
-    // Метод для получения значения по ключу
-    T Get(string key);
+    void Add(string key, T value){
+        int index = hashFunc(key);
+        
+        while (index < size){
+            if (!arr[index].state){
+                arr[index].key = key;
+                arr[index].value = value;
+                arr[index].state = true;
+                return;
+            } else if(arr[index].key == key){
+                arr[index].value = value;
+                return;
+            }
+            index++;
+        }
 
-    // Метод для удаления элемента по ключу
-    void Remove(string key);
+        Resize();
+        Add(key, value);
+    }
+
+    void Resize(){
+        size *= 2;
+        NodeH<T>* oldArr = arr;
+
+        arr = new NodeH<T>[size];
+
+        for(int i = 0; i < size / 2; i++){
+            if (oldArr[i].state && !oldArr[i].deleted){
+                Add(oldArr[i].key, oldArr[i].value);
+            }
+        }
+
+        delete[] oldArr;
+    }
+
+    T Get(string key){
+        int index = hashFunc(key);
+        
+        while (index < size){
+            if (arr[index].state && arr[index].key == key){
+                return arr[index].value;
+            } else if (arr[index].key == key && arr[index].deleted) {
+                throw runtime_error("No such key");
+            }else if (!arr[index].deleted && !arr[index].state) {
+                throw runtime_error("No such key");
+            }
+            index++;
+        }
+
+        throw runtime_error("No such key");
+    }
+
+    void Remove(string key){
+        int index = hashFunc(key);
+        while (index < size){
+            if (arr[index].state && arr[index].key == key){
+                if (is_same<T, List<string>>::value) {
+                    arr[index].value.clear();
+                }
+                arr[index].state = false;
+                arr[index].deleted = true;
+                return;
+            }else if (arr[index].key == key && arr[index].deleted) {
+                throw runtime_error("No such key");
+            }else if (!arr[index].deleted && !arr[index].state) {
+                throw runtime_error("No such key");
+            }
+            index++;
+        }
+        throw runtime_error("No such key");
+    }
 };
-
-#endif // DATA_STRUCTURES_H
